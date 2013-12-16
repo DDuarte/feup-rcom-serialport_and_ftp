54a62369c556b173de0a1a8ecfca2ca62715c49a
DDuarte <dnpd.dd@gmail.com>
2013-12-16 18:51:25 +0000

    ftp doxygen documentation

--- ftp/ftp.h
+++ ftp/ftp.h
@@ -3,23 +3,48 @@
 
 #include <stdlib.h>
 
+/**
+* @file ftp.h
+* @brief Partial implementation of a FTP connection as described in RFC959
+*
+* Note: Only the protocol required to download a file from a FTP server was implemented.
+*/
+
+/// FTP connection
 typedef struct FTP
 {
-    int control_socket_fd;
-    int data_socket_fd;
+    int control_socket_fd; ///< File descriptor of the control socket
+    int data_socket_fd; ///< File descriptor of the data socket
 } FTP;
 
+/// Creates a connection to a FTP server (does not send any command, only reads what is sent back from the server initially)
 int ftp_connect(FTP* FTP, const char* ip, int port);
+
+/// Sends USER and PASS commands to the FTP server
 int ftp_login(FTP* ftp, const char* user, const char* pass);
+
+/// Sends CWD (current working directory) command to the FTP server
 int ftp_cwd(FTP* ftp, const char* path);
+
+/// Sends PASV (passive mode) to the FTP server and creates a new data connection
 int ftp_pasv(FTP* ftp);
+
+/// Sends RETR to the FTP server
 int ftp_retr(FTP* ftp, const char* file);
+
+/// Sends QUIT to the FTP server and closes socket connections
 int ftp_disconnect(FTP* ftp);
 
+/// Retrieves a file from the FTP server (requires passive mode) and stores file in the local current working directory
 int ftp_download(FTP* ftp, const char* file_name);
 
+/// Auxiliary function to send a command but without reading the response from the FTP server
 int ftp_send_cmd_no_read(FTP* ftp, const char* cmd);
+
+/// Auxiliary function to send a command to the FTP server
 int ftp_send_cmd(FTP* ftp, const char* cmd);
+
+/// Auxiliary function to read from the FTP server
 int ftp_read(FTP* ftp, char* cmd, size_t size);
 
 #endif
--- ftp/url.h
+++ ftp/url.h
@@ -1,24 +1,37 @@
 #ifndef URL_H
 #define URL_H
 
-/* ftp://[<user>:<password>@]<host>[:port]/<url-path> */
+/**
+* @file url.h
+* @brief Representation of an URL as described in RFC1738
+*
+* ftp://[<user>:<password>@]<host>[:port]/<url-path>
+*/
 
 typedef char url_string[256];
 
+/// Structure to store the multiple parts of an URL
 typedef struct URL
 {
-    url_string user;
-    url_string password;
-    url_string host;
-    int port;
-    int num_parts;
-    url_string* parts;
-    url_string ip;
+    url_string user; ///< Username, empty if not specified (requires call to url_from_string)
+    url_string password; ///< Password, empty if not specified (requires call to url_from_string)
+    url_string host; ///< Hostname (requires call to url_from_string)
+    int port; ///< Port, 0 if not specified (requires call to url_from_string)
+    int num_parts; ///< Number of parts in the path of the URL (requires call to url_from_string)
+    url_string* parts; ///< Array with num_parts of parts in the path of the URL (requires call to url_from_string)
+    url_string ip; ///< IP Address (requires call to url_host_to_ip after url_from_string)
 } URL;
 
+/// Initialization of an empty URL
 void url_init(URL* url);
+
+/// Deallocate resources of an URL
 void url_destroy(URL* url);
+
+/// Parse a string with the url to fill in the URL structure
 int url_from_string(URL* url, const char* str);
+
+/// Converts the hostname (url->host) to its IPv4 address (url->ip)
 int url_host_to_ip(URL* url);
 
 #endif
